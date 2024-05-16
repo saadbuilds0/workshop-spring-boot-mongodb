5f41600dbf4c3897b4235f7bea2f24c84a177d59
Saad Khan <131930818+saadbuilds0@users.noreply.github.com>
2024-05-16 15:39:51 -0300

    Query with multiple criteria

--- src/main/java/com/rodrigodantas/workshopmongo/resources/PostResources.java
+++ src/main/java/com/rodrigodantas/workshopmongo/resources/PostResources.java
@@ -1,5 +1,6 @@
 package com.rodrigodantas.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,18 @@ public class PostResources {
 	public ResponseEntity<List<Post>> findByIdTitle(@RequestParam(value="text",defaultValue = "") String text){
 		text = URL.decodeParam(text);
 		List<Post> list = service.fingByTitle(text);
+		
+		return ResponseEntity.ok().body(list);
+	}
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text",defaultValue = "") String text,
+			@RequestParam(value="minDate",defaultValue = "") String minDate,
+			@RequestParam(value="maxDate",defaultValue = "") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(minDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		
 		return ResponseEntity.ok().body(list);
 	}
--- src/main/java/com/rodrigodantas/workshopmongo/resources/util/URL.java
+++ src/main/java/com/rodrigodantas/workshopmongo/resources/util/URL.java
@@ -2,8 +2,13 @@ package com.rodrigodantas.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
+	
 	public static String decodeParam(String text) {
 		try {
 			return URLDecoder.decode(text, "UTF-8");
@@ -12,5 +17,15 @@ public class URL {
 		}
 	}
 	
+	public static Date convertDate(String textDate, Date defaltValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaltValue;
+		}
+		
+	}
 	
 }
